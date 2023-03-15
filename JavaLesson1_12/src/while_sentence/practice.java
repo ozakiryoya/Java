9a6c3261256134829b2f7c08297789af6f6fb1d9
ozaki <ozaki@ozaki.lan>
2023-03-15 22:25:13 +0900

    JavaLesson1.12 commit

---
+++ JavaLesson1_12/src/while_sentence/practice.java
@@ -0,0 +1,32 @@
+package while_sentence;
+
+public class practice {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		/*
+		問題．while文を使用し、以下のプログラムを作成してください。
+			・1～100を出力
+			・奇数の数字に「〇は奇数です」と出力
+			・偶数の数字に「○は偶数です」と出力
+			  出力結果  1は奇数です
+						2は偶数です
+						3は奇数です
+						4は偶数です
+							・
+							・
+							・
+						100は偶数です
+	*/
+		int num = 1;
+		while(num <= 100) {
+			if(num % 2 == 0) {
+				System.out.println(num + "は偶数です");
+			}else {
+				System.out.println(num + "は奇数です");
+			}
+			num++;
+		}
+	}
+
+}
